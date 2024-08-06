f2f4968e0c42af1a8d337730e779cc96ebb4e00a
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2024-08-07 00:29:41 +0700

    updated dependencies

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,49 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const connectDB_1 = __importDefault(require("./utils/connectDB"));
 const createApp_1 = require("./utils/createApp");
 const logger_1 = __importDefault(require("./utils/logger"));
 const app = (0, createApp_1.createApp)();
 const PORT = process.env.SERVER_PORT || 3002;
-app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
+app.listen(PORT, () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     logger_1.default.info(`Listening at http://localhost:${PORT}`);
     yield (0, connectDB_1.default)();
-}));
+  })
+);
